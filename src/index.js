83b6afb22b80380eaf2559cbb78f98458d01ea09
yamaanDB <yamaan@doodleblue.in>
2024-08-16 19:12:59 +0530

    Fixed3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
 import ReactDOM from 'react-dom';
 import Routes from "./routes/index";
 import { Provider } from "react-redux";
